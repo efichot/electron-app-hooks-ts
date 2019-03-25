f4bc935623fdc4e6be6ea76a2d23c346617efb9e
efichot <fichotetienne@gmail.com>
2019-03-25 14:51:04 +0700

    Add symbolic link for db and absolute path with electron ✅

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,12 @@
+import electron from "electron";
 import low from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
+import path from "path";
+
+const { app } = electron.remote;
 
 // DB Init
-const adapter = new FileSync("db.json", {
+const adapter = new FileSync(path.join(app.getPath("userData"), "db.json"), {
   defaultValue: { keys: [] }
 });
 const db = low(adapter);
