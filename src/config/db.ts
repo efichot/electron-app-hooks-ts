3eb94a9ed6e5b675da4a222cb6a88a94997af5e6
efichot <fichotetienne@gmail.com>
2019-03-27 17:54:24 +0700

    Encrypt db ✅

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,6 +8,8 @@ const { app } = electron.remote;
 // DB Init
 const adapter = new FileSync(path.join(app.getPath("userData"), "db.json"), {
   defaultValue: { keys: [] }
+  // serialize: data => btoa(JSON.stringify(data)),
+  // deserialize: data => JSON.parse(atob(data))
 });
 const db = low(adapter);
 
